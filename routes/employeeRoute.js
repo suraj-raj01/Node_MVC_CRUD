ca94bd361987122e6a32fee08b27973c866808b3
SURAJ_KUMAR <surajk38656@gmail.com>
2024-12-27 00:28:39 +0530

    EmpRoutes

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,13 +1,16 @@
+// Import the required libraries and modules.
 const express = require("express");
 const route = express.Router();
-const empController = require("../controller/empController")
+const empController = require("../controller/empController");
 
+// Define routes and map them to controller functions.
 route.get("/", empController.empHome);
 route.get("/insert", empController.empInsert);
 route.get("/display", empController.empDisplay);
 route.get("/contact", empController.empContact);
 route.post("/datasave", empController.empDataSave);
-route.get("/update",empController.dataUpdate);
-route.get("/recdel",empController.recDelete)
+route.get("/update", empController.dataUpdate);
+route.get("/recdel", empController.recDelete);
 
+// Export the route module.
 module.exports = route;
